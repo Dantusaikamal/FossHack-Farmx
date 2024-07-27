414643f7a1af88f4457501dee1e8d41429c04230
dantusaikamal <dantusaikamal@gmail.com>
2024-07-27 22:20:52 +0530

    fix: Reverted main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:farmx/helper/local_string.dart';
+import 'package:farmx/services/disease_provider.dart';
+import 'package:farmx/src/home_page/home.dart';
+import 'package:farmx/src/home_page/models/disease_model.dart';
+import 'package:farmx/src/onboading.dart';
+import 'package:farmx/src/suggestions_page/suggestions.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MainApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(DiseaseAdapter());
+
+  await Hive.openBox<Disease>('plant_diseases');
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+
+  runApp(
+    MyApp(),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return ChangeNotifierProvider<DiseaseService>(
+      create: (context) => DiseaseService(),
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        translations: LocalString(),
+        locale: Get.deviceLocale,
+        title: 'Detect diseases',
+        theme: ThemeData(primarySwatch: Colors.green, fontFamily: 'SFRegular'),
+        onGenerateRoute: (RouteSettings routeSettings) {
+          return MaterialPageRoute<void>(
+            settings: routeSettings,
+            builder: (BuildContext context) {
+              switch (routeSettings.name) {
+                case Suggestions.routeName:
+                  return const Suggestions();
+                case Home.routeName:
+                default:
+                  return const OnBoadingScreen();
+              }
+            },
+          );
+        },
       ),
     );
   }
