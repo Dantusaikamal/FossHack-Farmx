87735291895e3bb127427b2f356817ffaafb9817
dantusaikamal <dantusaikamal@gmail.com>
2024-07-27 22:48:30 +0530

    fix: Fixed assets loading issue

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,12 @@ Future main() async {
       [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
 
   runApp(
-    MyApp(),
+    const MyApp(),
   );
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
